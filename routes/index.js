46dcf682e49aded6ef84749262bc7d69043dc60b
Colin Murphy <murphcol@oregonstate.edu>
2018-08-06 19:09:58 -0700

    Converted view manager from jadade to pug and created an example layout

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Login or view store map' });
+});
+
+router.get('/map', function(req, res, next) {
+  res.render('map', { title: 'Stores near you' });
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,20 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('user home page');
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Login here' });
+});
+
+router.get('/order', function(req, res, next) {
+  res.render('order', { title: 'Place an order' });
+});
+
+router.post('/order', function(req, res, next) {
+  var order = req.body;
+  res.render('order submitted!');
 });
 
 module.exports = router;
