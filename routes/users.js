2437040a379cc4e549d26cbe0b497772e19dcee9
Colin Murphy <murphcol@oregonstate.edu>
2018-08-10 17:43:18 -0700

    Added registration interfaces

--- routes/users.js
+++ routes/users.js
@@ -3,11 +3,29 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('user home page');
+  res.render('users', { title: 'User homepage' });
 });
 
 router.get('/login', function(req, res, next) {
   res.render('login', { title: 'Login here' });
+});
+
+router.get('/registerUser', function(req, res, next) {
+  res.render('register-user', { title: 'Register a new user' });
+});
+
+router.post('/registerUser', function(req, res, next) {
+  var user = req.body;
+  res.render('User created!');
+});
+
+router.get('/registerStore', function(req, res, next) {
+  res.render('register-user', { title: 'Register a new store' });
+});
+
+router.post('/registerStore', function(req, res, next) {
+  var user = req.body;
+  res.render('Store created!');
 });
 
 router.get('/order', function(req, res, next) {
